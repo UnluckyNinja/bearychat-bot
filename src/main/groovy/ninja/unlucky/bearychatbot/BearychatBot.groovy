21b0a7a081c43919649a9a71b453c3d48320af58
UnluckyNinja <unluckyninja1994@gmail.com>
2015-10-15 03:02:29 +0800

    debug price not present

--- src/main/groovy/ninja/unlucky/bearychatbot/BearychatBot.groovy
+++ src/main/groovy/ninja/unlucky/bearychatbot/BearychatBot.groovy
@@ -72,6 +72,7 @@ public class BearychatBot extends GroovyVerticle {
                                     color = '#9E9E9E'
                                     first().select('b').text()
                                 }
+                                log.debug child.select('td.price-final').html()
                                 def price = child.select('td.price-final').text()
                                 def logo = child.select('td.applogo').select('a').first().attr('href').replaceFirst($/(/(app|sub))(/\d+/)/$) {
                                     it[1] + 's' + it[3]
